d7e561dcf319ed8f2eaf4bcad49e80cae1625028
flakegotgame <victorchange808@gmail.com>
2024-04-26 23:43:15 +0300

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,5 +5,16 @@ function Home() {
   // update the JSX being returned!
   return <div>Home</div>;
 }
+  return(
+    <div id="home">
+
+      <h1 style={{color: "firebrick"}}>
+           {name} is a Web Developer from {city}
+      </h1>
+
+    </div>
+  );
+;
+
 
 export default Home;
